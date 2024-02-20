a7ea0e3571370796c951e10d1d485182079133bf
Danilo Affonso <dluzrios@gmail.com>
2024-02-20 14:37:45 -0300

    Analisa situacao dos alunos

---
+++ nota.js
@@ -0,0 +1,42 @@
+const alunos = [];
+class Alunos {
+    situacao = '';
+    constructor(nomeDoAluno,notaDoAluno){
+        this.nome = nomeDoAluno;
+        this.nota = notaDoAluno;
+    }
+    
+    analiseDeSituacao(){
+        if(this.nota >= 6){
+            this.situacao = 'Aprovado';
+            return `O aluno ${this.nome} foi aprovado com uma nota maior que 6`;
+        } else {
+            this.situacao = 'Reprovado';
+            return `O aluno ${this.nome} foi reprovado com uma nota menor que 6`;
+        }
+    }
+}
+
+const aluno1 = new Alunos('João', 7);
+const aluno2 = new Alunos('Maria', 8);
+const aluno3 = new Alunos('Pedro', 5);
+const aluno4 = new Alunos('Lucas', 10);
+
+aluno1.analiseDeSituacao();
+aluno2.analiseDeSituacao();
+aluno3.analiseDeSituacao();
+aluno4.analiseDeSituacao();
+
+alunos.push(aluno1,aluno2,aluno3,aluno4);
+
+function alunosComNotaMaiorQueSeis(){
+    for(let i = 0; i < alunos.length; i++){
+        if(alunos[i].situacao == 'Aprovado'){
+            console.log(`${alunos[i].nome} - Aprovado`)
+        }
+    }
+}
+
+alunosComNotaMaiorQueSeis();
+
+
