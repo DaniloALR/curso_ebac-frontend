ab835c95baeb9d8988d9b1a9e05d52de3b7a7c40
Danilo Affonso <dluzrios@gmail.com>
2024-02-21 13:29:09 -0300

    exercicio_ts

---
+++ src/ex_typescript.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(numeroA, numeroB) {
+    return numeroA * numeroB;
+}
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplica(4, 2));
+console.log(dizOla('Danilo'));
---
+++ src/ex_typescript.ts
@@ -0,0 +1,7 @@
+function multiplica(numeroA:number,numeroB:number):number{
+    return numeroA * numeroB
+}
+
+function dizOla(nome:string):string{
+    return `Olá ${nome}`
+}
